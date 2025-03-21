c1cbe01fcdf9892149b1ed2c1f7bb830e702f0a8
bryan <aseckrr05@gmail.com>
2025-03-21 17:53:53 +0100

    interface frontEnd change by FrontAlejandro

---
+++ FronEnd-ANGULAR/src/app/professional/professional-header/professional-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './professional-header.component.html',
+  styleUrl: './professional-header.component.scss'
+})
+export class ProfessionalHeaderComponent {
+
+}
