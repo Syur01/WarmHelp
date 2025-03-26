f8b3c346b216c7c6c62d3a5a0a2598096fd0a7fc
alejDev <aseckrr05@gmail.com>
2025-03-26 13:02:28 +0100

    create backendSB

---
+++ backendSB/src/main/java/com/warmhelp/app/WarmHelpApplication.java
@@ -0,0 +1,13 @@
+package com.warmhelp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmHelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmHelpApplication.class, args);
+	}
+
+}
