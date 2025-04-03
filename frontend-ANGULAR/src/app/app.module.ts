2c99cf8bd8a7ff07fc4e421d6f18a673a4e2e4fa
alejDev <aseckrr05@gmail.com>
2025-04-03 16:16:41 +0200

    new angular project

--- frontend-ANGULAR/src/app/app.module.ts
+++ frontend-ANGULAR/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BackofficeAdminComponent } from './admin/backoffice-admin/backoffice-admin.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { HomeNoAuthComponent } from './user/home-no-auth/home-no-auth.component';
+import { LayoutComponent } from './user/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackofficeAdminComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeNoAuthComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
