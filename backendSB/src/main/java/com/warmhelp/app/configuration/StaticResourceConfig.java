897ec35eada54da77410ca0685f3e2e9ebf4fe0b
bryan <aseckrr05@gmail.com>
2025-05-06 23:46:45 +0200

    url

--- backendSB/src/main/java/com/warmhelp/app/configuration/StaticResourceConfig.java
+++ backendSB/src/main/java/com/warmhelp/app/configuration/StaticResourceConfig.java
@@ -12,4 +12,6 @@ public class StaticResourceConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/uploads/images/**")
                 .addResourceLocations("file:C:/JavaStuff/Angular/WarmHelp/WarmHelp/backendSB/uploads/images/");
     }
+
+    // C:/Users/aleja/Desktop/WarmHelp/backendSB/uploads/images/ ruta alejandro
 }
