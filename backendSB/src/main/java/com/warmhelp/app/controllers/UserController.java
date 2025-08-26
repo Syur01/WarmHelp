983fc5c8ef27666df3e423391055b958da0a64a0
syur <bp113534@email.com>
2025-08-26 22:08:04 +0200

    arreglo de cors

--- backendSB/src/main/java/com/warmhelp/app/controllers/UserController.java
+++ backendSB/src/main/java/com/warmhelp/app/controllers/UserController.java
@@ -26,7 +26,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin("*")
+//@CrossOrigin("*")
 public class UserController {
 
     private final UserService userService;
