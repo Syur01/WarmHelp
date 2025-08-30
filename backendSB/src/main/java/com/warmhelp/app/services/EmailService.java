7ccaedb688f79ad7fe2f76d11ea2d2279f725780
syur <bp113534@email.com>
2025-08-30 22:52:10 +0200

    arreglo de email

--- backendSB/src/main/java/com/warmhelp/app/services/EmailService.java
+++ backendSB/src/main/java/com/warmhelp/app/services/EmailService.java
@@ -3,15 +3,17 @@ package com.warmhelp.app.services;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
 import java.util.*;
-
 @Service
-public class EmailService {
+public class EmailService implements IEmailService {
 
-    private final String apiKey = System.getenv("BREVO_API_KEY"); // API Key de Brevo
-    private final String fromEmail = System.getenv("FROM_EMAIL"); // Email remitente
+    private final String apiKey = System.getenv("BREVO_API_KEY");
+    private final String fromEmail = System.getenv("FROM_EMAIL");
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Override
     public void sendEmail(String[] toEmails, String subject, String htmlContent) {
         String url = "https://api.brevo.com/v3/smtp/email";
 
@@ -31,7 +33,12 @@ public class EmailService {
         headers.set("api-key", apiKey);
 
         HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
-
         restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
     }
+
+    @Override
+    public void sendEmailWithFile(String[] toUser, String subject, String message, File file) {
+
+    }
 }
+
