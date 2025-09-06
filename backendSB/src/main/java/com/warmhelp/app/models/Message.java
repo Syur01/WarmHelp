372fadbf479d8a0182f01c0ec1ae52591cd14852
syur <bp113534@email.com>
2025-09-06 23:08:25 +0200

    INTENTO

--- backendSB/src/main/java/com/warmhelp/app/models/Message.java
+++ backendSB/src/main/java/com/warmhelp/app/models/Message.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class Message {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String content;
