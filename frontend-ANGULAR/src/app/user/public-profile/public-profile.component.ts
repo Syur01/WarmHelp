54b0858685712444474e97d79385f561f1fe937a
syur <bp113534@email.com>
2025-09-07 15:23:42 +0200

    arreglo url

--- frontend-ANGULAR/src/app/user/public-profile/public-profile.component.ts
+++ frontend-ANGULAR/src/app/user/public-profile/public-profile.component.ts
@@ -75,14 +75,14 @@ export class PublicProfileComponent implements OnInit {
       return '/ken.gif'; // Imagen por defecto
     }
 
-  const cleanPath = avatarPath.trim(); // 🔑 recorta espacios
+    const cleanPath = avatarPath.trim(); // 🔑 recorta espacios
 
-  if (cleanPath.startsWith('http://') || cleanPath.startsWith('https://')) {
-    return cleanPath; // usar Cloudinary directamente
-  }
+    if (cleanPath.startsWith('http://') || cleanPath.startsWith('https://')) {
+      return cleanPath; // usar Cloudinary directamente
+    }
 
-  // Si es relativa, concatena con tu API
-  return `${environment.apiUrl}${cleanPath}`;
+    // Si es relativa, concatena con tu API
+    return `${cleanPath}`;
   }
   getImageUrl(imagePath: string): string {
     if (!imagePath || typeof imagePath !== 'string')
