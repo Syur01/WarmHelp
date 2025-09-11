7be5831cecbd1f25906d1c6887e51243d55812dc
syur <bp113534@email.com>
2025-09-11 17:56:27 +0200

    arreglo de upload avatar

--- frontend-ANGULAR/src/app/services/interfaces/auth.ts
+++ frontend-ANGULAR/src/app/services/interfaces/auth.ts
@@ -1,30 +1,15 @@
-export interface UserInterface{
-    idUser: number,
-    username: string,
-    password: string,
-    roleType: "CLIENT"|"PROFESSIONAL"|"ADMIN",
-    first_name: string,
-    last_name: string,
-    address: string,
-    number: string,
-    email: string,
-    avatar?: string;
-    mySelf_description?: string
-}
-mapUserToInterface(user: User): UserInterface {
-  return {
-    idUser: user.id,
-    username: user.username,
-    password: user.password,
-    roleType: user.role?.name || "CLIENT", // ejemplo
-    first_name: user.userInfo?.first_name || "",
-    last_name: user.userInfo?.last_name || "",
-    address: user.userInfo?.address || "",
-    number: user.userInfo?.number || "",
-    email: user.email,
-    avatar: user.userInfo?.avatar,
-    mySelf_description: user.userInfo?.mySelf_description
-  };
+export interface UserInterface {
+  idUser: number;
+  username: string;
+  password: string;
+  roleType: 'CLIENT' | 'PROFESSIONAL' | 'ADMIN';
+  first_name: string;
+  last_name: string;
+  address: string;
+  number: string;
+  email: string;
+  avatar?: string;
+  mySelf_description?: string;
 }
 
-export type LoginInterface = Pick<UserInterface, "username"|"password">
+export type LoginInterface = Pick<UserInterface, 'username' | 'password'>;
